4f29607118e580336e4c6f6b65b5fece02966570
dillon <dillon@163.com>
2021-02-14 13:26:28 +0800

    商品模块提交代码

---
+++ product9998/src/main/java/com/wudi/Product9998Application.java
@@ -0,0 +1,13 @@
+package com.wudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Product9998Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Product9998Application.class, args);
+    }
+
+}
--- product9998/src/main/java/com/wudi/controller/ProductController.java
+++ product9998/src/main/java/com/wudi/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.wudi.controller;
 
+import com.netflix.loadbalancer.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Dillon Wu
@@ -12,10 +15,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class ProductController {
+    private static AtomicInteger nextServerCyclicCounter;
 
-    @GetMapping("/product/showMsg")
+
+    @GetMapping("/product/show/msg")
     public String showMsg(){
         log.info("进入商品服务,展示商品----");
+
         return "进入商品服务，展示商品";
+    }
+
+    public static void main(String[] args) {
+     /*int number =   1%4;
+        System.out.println("==:"+number);*/
+     //测试AtomicInteger
+         nextServerCyclicCounter = new AtomicInteger(0);
+        //期望值是1更改值是2
+        int i = incrementAndGetModulo(4);
+        System.out.println(i);
+    }
+    private static int incrementAndGetModulo(int modulo) {
+        int current;
+        int next;
+        do {
+            current = nextServerCyclicCounter.get();
+            System.out.println("===="+current);
+            next = (current + 1) % modulo;
+        } while(!nextServerCyclicCounter.compareAndSet(current, next));
+
+        return next;
     }
 }

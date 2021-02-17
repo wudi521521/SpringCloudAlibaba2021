0ac81c06f8b49e8ac0179e872ceb5b376d95fd90
dillon <dillon@163.com>
2021-02-17 18:18:07 +0800

    gateway

---
+++ consulclient8889/src/main/java/com/wudi/Gateway8500Application.java
@@ -0,0 +1,13 @@
+package com.wudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gateway8500Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway8500Application.class, args);
+    }
+
+}

9912ddb81ea762e20ea216d302315a3e7349813f
dillon <dillon@163.com>
2021-02-20 20:00:50 +0800

    数据结构算法-leetcode

---
+++ leetcode6161/src/main/java/com/wudi/Leetcode6161Application.java
@@ -0,0 +1,13 @@
+package com.wudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leetcode6161Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Leetcode6161Application.class, args);
+    }
+
+}
---
+++ leetcode6161/src/test/java/com/wudi/Leetcode6161ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Leetcode6161ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
